9674eec146792af2fea3bd875b1edcb19efebf2c
Filip Ralev <filipralev@yahoo.com>
2026-05-13 22:58:35 +0200

    Update Vite base path to /brioni/

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/brioni-legacy-site/",
+  base: "/brioni/",
   server: {
     host: "::",
     port: 8080,
